ddb44aad4f7d0154cca3521477947aa3a21c1d0e
Nakshatra Gupta <nakshatragupta007@gmail.com>
2023-05-30 16:26:51 +0530

    Intents added for switching activities.

--- app/src/main/java/com/example/adrdeweatherapp/MainActivity.java
+++ app/src/main/java/com/example/adrdeweatherapp/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.adrdeweatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button CurrentLocationButton;
+    private Button OtherLocationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CurrentLocationButton = findViewById(R.id.button_current_location);
+        OtherLocationButton = findViewById(R.id.button_manual_location);
+
+        CurrentLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Fetching Current Location....", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        OtherLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OtherLocation.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/adrdeweatherapp/OtherLocation.java
+++ app/src/main/java/com/example/adrdeweatherapp/OtherLocation.java
@@ -3,12 +3,53 @@ package com.example.adrdeweatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class OtherLocation extends AppCompatActivity {
+
+    private EditText latitudeEditText;
+    private EditText longitudeEditText;
+    private Button doneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_location);
+
+        latitudeEditText = findViewById(R.id.EditText_latitude);
+        longitudeEditText = findViewById(R.id.EditText_longitude);
+        doneButton = findViewById(R.id.button_done);
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String latitude = latitudeEditText.getText().toString();
+                String longitude = longitudeEditText.getText().toString();
+
+                FetchData(latitude, longitude);
+            }
+        });
+    }
+
+    private void FetchData(String latitude, String longitude) {
+
+        if(TextUtils.isEmpty(latitude))
+        {
+            Toast.makeText(this, "Please enter the Latitude.", Toast.LENGTH_SHORT).show();
+        }
+        else
+        if(TextUtils.isEmpty(longitude))
+        {
+            Toast.makeText(this, "Please enter the Longitude.", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            Toast.makeText(this, "Fetching data. Please wait for a while.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
